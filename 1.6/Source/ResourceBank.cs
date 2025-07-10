9c6f3cb1f5a0598aceed89c4878f843ca06c57a2
John Cannon <john.arc.cannon@gmail.com>
2025-07-10 17:37:09 +0100

    Recompile for 1.6 added dds textures

---
+++ 1.6/Source/ResourceBank.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Policy;
+using UnityEngine;
+using Verse;
+
+// ReSharper disable All
+
+namespace WallShields
+{
+    [StaticConstructorOnStartup]
+    public static class ResourceBank
+    {
+        public const string modName = "WallStuff";
+        public static string NeedsWall = "Can only be placed on a wall";
+        public static string WallAlreadyOccupied = "Space already occupied";
+        public static string SpaceInFrontOfWallNotClear = "Space infront of wall not clear";
+        public static string NoReflectorFound = "No Laser Grid Reflector Found In Range";
+    }
+}
